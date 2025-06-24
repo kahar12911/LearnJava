485433e5d02f630c644d22a67f2ed89807686809
kahar12911 <kahar12911@gmail.com>
2025-06-24 17:22:25 +0530

    String Buffer

---
+++ src/main/java/org/stringBuilder/StringBuilderThread.java
@@ -0,0 +1,22 @@
+package org.stringBuilder;
+
+public class StringBuilderThread {
+    static StringBuilder sb = new StringBuilder("Hello ");
+
+    public static void main(String[] args) {
+        Runnable task = () -> {
+            for(int i = 0 ; i < 5; i++){
+                sb.append(i);
+                System.out.println(sb);
+            }
+        };
+
+        Thread t1 = new Thread(task);
+        Thread t2 = new Thread(task);
+        t1.start();
+        t2.start();
+//        t1.run();
+//        t2.run();
+        System.out.println("sb " +sb);
+    }
+}
